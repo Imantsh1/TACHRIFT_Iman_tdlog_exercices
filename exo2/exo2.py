d14b72e068b0b23fd54695b3ce91ef5947eca3d6
Imantsh1 <140084705+Imantsh1@users.noreply.github.com>
2023-10-10 15:41:43 +0100

    Update exo2.py

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,33 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+def solution(string, ending):
+    return string.endswith(ending)
+
+# Unit tests for true cases
+fixed_tests_True = (
+    ("samurai", "ai"),
+    ("ninja", "ja"),
+    ("sensei", "i"),
+    ("abc", "abc"),
+    ("abcabc", "bc"),
+    ("fails", "ails"),
+)
+
+for test in fixed_tests_True:
+    result = solution(test[0], test[1])
+    print(f"Input: ({test[0]}, {test[1]}) => Output: {result} (Expected: True)")
+
+# Unit tests for false cases
+fixed_tests_False = (
+    ("sumo", "omo"),
+    ("samurai", "ra"),
+    ("abc", "abcd"),
+    ("ails", "fails"),
+    ("this", "fails"),
+    ("spam", "eggs"),
+)
+
+for test in fixed_tests_False:
+    result = solution(test[0], test[1])
+    print(f"Input: ({test[0]}, {test[1]}) => Output: {result} (Expected: False)")
